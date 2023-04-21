0f977f0944a682a63f8a7bce68fea3ddb9e1ed9b
Zhomart <okassymzhomart@mail.ru>
2023-04-21 17:22:01 +0600

    Lesson_2 function

---
+++ js.js
@@ -0,0 +1,38 @@
+// let num = 20;
+
+// function showFirstMessage (text) {
+//     alert(text);
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello world");
+// console.log(num);
+
+
+
+// let calc = function calc(a,b) {
+//     return (a+b);
+// }
+ 
+ let calc = (a,b) => a+b 
+
+console.log(calc(3,4));
+console.log(calc(8,4));
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+
+let anotherNum = retVar(); 
+console.log(anotherNum);
+
+let str = "test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.2";
+
+console.log(Math.round(twelve));
--- script.js
+++ script.js
@@ -21,7 +21,8 @@ for (let i = 0;i < 2; i++ ) {
 
     }
     else {
-
+        console.log ("bad result");
+        i--;
     }
      
 }
