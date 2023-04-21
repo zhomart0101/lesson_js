e1737e1cc483cae5d08b2db81061b286ef993b43
Zhomart <okassymzhomart@mail.ru>
2023-04-21 17:24:52 +0600

    Lesson_2 function

--- js.js
+++ js.js
@@ -33,6 +33,9 @@ console.log(str.length);
 console.log(str.toUpperCase());
 console.log(str.toLowerCase());
 
-let twelve = "12.2";
+let twelve = "12.2px";
 
-console.log(Math.round(twelve));
+//console.log(Math.round(twelve));
+
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
