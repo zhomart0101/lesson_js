7001b0e1dd02bec94aec2ce3886c2807bf63fe77
zhomart0101 <128991929+zhomart0101@users.noreply.github.com>
2023-04-23 22:45:11 +0600

    Lesson_3 callback

--- js.js
+++ js.js
@@ -14,28 +14,42 @@
 //     return (a+b);
 // }
  
- let calc = (a,b) => a+b 
+//  let calc = (a,b) => a+b 
 
-console.log(calc(3,4));
-console.log(calc(8,4));
+// console.log(calc(3,4));
+// console.log(calc(8,4));
 
-function retVar() {
-    let num = 50;
-    return num;
+// function retVar() {
+//     let num = 50;
+//     return num;
+// }
+
+// let anotherNum = retVar(); 
+// console.log(anotherNum);
+
+// let str = "test";
+// console.log(str.length);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// let twelve = "12.2px";
+
+// //console.log(Math.round(twelve));
+
+// console.log(parseInt(twelve));
+// console.log(parseFloat(twelve));
+
+function first() {
+    //Что-то делаем
+    setTimeout(function() {
+        console.log(1);
+    },500);
 }
 
-let anotherNum = retVar(); 
-console.log(anotherNum);
+function second () {
+    console.log(2);
+}
 
-let str = "test";
-console.log(str.length);
-
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-
-let twelve = "12.2px";
-
-//console.log(Math.round(twelve));
-
-console.log(parseInt(twelve));
-console.log(parseFloat(twelve));
+first();
+second();
